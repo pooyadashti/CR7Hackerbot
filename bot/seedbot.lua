a807a60b65ff3b70e7e469642bdbba45b6ad6949
CR7Hacker <pooyacr7realmadrid@gmail.com>
2016-01-28 09:41:32 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all",
     "leave_ban"
-    },
+    sudo_users={cr7hacker},--sudo users
     sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
